0ab1c16bf0dc8fe6d3bf748584baeeb8e8269fac
IheCharles <cci29@drexel.edu>
2024-01-20 11:02:07 -0500

    l

---
+++ public/index.js
@@ -0,0 +1,98 @@
+import firebase from "/firebase/compat/app";
+import "firebase/compat/auth";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ3sc83x5CcAXW9NTt-NJUcT6C1Zzk6Fc",
+  authDomain: "artify-22dff.firebaseapp.com",
+  projectId: "artify-22dff",
+  storageBucket: "artify-22dff.appspot.com",
+  messagingSenderId: "287489070433",
+  appId: "1:287489070433:web:83767829fbbb878168e120",
+  measurementId: "G-TVHZLDMK8L"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+document.getElementById('nextPageButton').addEventListener('click', function() {
+    window.location.href = 'login.html'; // Replace with your desired URL
+  });
+let bubblez = SVG("#maskBubblez");
+let numOfBubblez = 50;
+
+let circles = [{ x: 0, y: 0, radius: 0 }];
+
+for (let i = 0; i < numOfBubblez; i++) {
+	function drawCircleWithoutOverlap() {
+		let newX = gsap.utils.random(0, 100);
+		let newY = gsap.utils.random(0, 200);
+		let newRadius = gsap.utils.random(4, 20);
+		let newCircle = { x: newX, y: newY, radius: newRadius };
+		let isOverlapping = false;
+
+		circles.forEach((circle, i) => {
+			let deltaX = newCircle.x - circles[i].x;
+			let deltaY = newCircle.y - circles[i].y;
+			let dist = Math.hypot(deltaX, deltaY);
+			let radiiiis = circles[i].radius + newCircle.radius;
+
+			if (dist < radiiiis) {
+				isOverlapping = true;
+			}
+		});
+
+		if (isOverlapping) {
+			drawCircleWithoutOverlap();
+		} else {
+			bubblez
+				.circle(newCircle.radius)
+				.x(newCircle.x)
+				.y(newCircle.y)
+				.fill("#fff")
+				.opacity(newCircle.radius / 20);
+
+			circles.push(newCircle);
+		}
+	}
+
+	drawCircleWithoutOverlap();
+}
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("circle", {
+	y: () => 1 - gsap.utils.random(0.1, 0.4) * (ScrollTrigger.maxScroll(window)/4),
+	ease: "none",
+	scrollTrigger: {
+		start: 0,
+		end: "max",
+		invalidateOnRefresh: true,
+		scrub: 2
+	}
+});
+
+
+document.getElementById('logoutButton').addEventListener('click', function() {
+	auth.signOut().then(() => {
+	  // Sign-out successful.
+	}).catch((error) => {
+	  // An error happened.
+	  console.error('Logout error:', error.message);
+	});
+});
+  
+auth.onAuthStateChanged(function(user) {
+	const welcomeElement = document.getElementById('welcome');
+	const logoutButtonElement = document.getElementById('logoutButton');
+	if (user) {
+	  // User is signed in.
+	  welcomeElement.style.display = 'block';
+	  logoutButtonElement.style.display = 'block';
+	} else {
+	  // No user is signed in.
+	  welcomeElement.style.display = 'none';
+	  logoutButtonElement.style.display = 'none';
+	}
+  });
---
+++ public/login.js
@@ -0,0 +1,90 @@
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+import { getAuth,signInWithPopup,setPersistence,browserLocalPersistence ,createUserWithEmailAndPassword,signInWithEmailAndPassword,GoogleAuthProvider    } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
+const isVerified = true;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ3sc83x5CcAXW9NTt-NJUcT6C1Zzk6Fc",
+  authDomain: "artify-22dff.firebaseapp.com",
+  projectId: "artify-22dff",
+  storageBucket: "artify-22dff.appspot.com",
+  messagingSenderId: "287489070433",
+  appId: "1:287489070433:web:83767829fbbb878168e120",
+  measurementId: "G-TVHZLDMK8L"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+var provider = new GoogleAuthProvider();
+provider.addScope('profile');
+provider.addScope('email');
+document.getElementById("signup-link").addEventListener("click", function() {
+  document.getElementById("sign-in-button").style.display = "none";
+  document.getElementById("sign-up-button").style.display = "block";
+  document.getElementById("reenter-password").style.display = "block";
+});
+
+document.getElementById('google-login-button').addEventListener('click', function() {
+    setPersistence(auth, browserLocalPersistence)
+      .then(() => {
+        
+        signInWithPopup(auth,provider).then(() => {
+        window.location.href = 'index.html';
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        
+        const errorMessageElement = document.getElementById('error-message');
+        errorMessageElement.style.display = 'block';
+        errorMessageElement.textContent = errorMessage;
+      });
+  });
+})
+
+
+document.getElementById('sign-up-button').addEventListener('click', function() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const reenterPassword = document.getElementById('reenter-password').value;
+
+    if (password !== reenterPassword) {
+        document.getElementById('error-message').textContent = 'Passwords do not match';
+        return;
+    }
+
+    setPersistence(auth, browserLocalPersistence)
+      .then(() => {
+        return createUserWithEmailAndPassword(auth, email, password);
+      })
+      .then(() => {
+
+        window.location.href = 'getstarted.html';
+      })
+      .catch((error) => {
+        errorMessageElement.style.display = 'block';
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        document.getElementById('error-message').textContent = errorMessage;
+      });
+});
+
+document.getElementById('sign-in-button').addEventListener('click', function() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    setPersistence(auth, browserLocalPersistence)
+      .then(() => {
+
+        return signInWithEmailAndPassword(auth, email, password);
+      })
+      .then(() => {
+
+        window.location.href = 'index.html';
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        
+        const errorMessageElement = document.getElementById('error-message');
+        errorMessageElement.style.display = 'block';
+        errorMessageElement.textContent = errorMessage;
+      });
+});
